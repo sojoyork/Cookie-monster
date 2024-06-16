3864b34b421513bc039da8b99f3ed6fa7a567c71
sojoyork <aimaankhankvsworkmail@gmail.com>
2024-06-16 07:43:27 -0700

    Update Cookie_news_adder.py

--- Cookie_monster.folder/Cookie_news_adder.py
+++ Cookie_monster.folder/Cookie_news_adder.py
@@ -5,7 +5,7 @@ def add_news_to_file(news, filename='cookie_news.txt'):
 
 def print_welcome_message():
     print("Welcome to Cookie News Adder! <3")
-    print("Only developers can use this tool to add news and updates about Cookie Monster.")
+    print("Only developers can use this tool to tell news and updates about Cookie Monster.")
 
 if __name__ == "__main__":
     print_welcome_message()
